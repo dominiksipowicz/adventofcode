aa3cd00a86625fe12b76ce45d07182ef1efce920
Dominik Sipowicz <dominik.sipowicz@gmail.com>
2021-12-02 10:36:33 +0000

    day2 task 2

--- 2021/day2.js
+++ 2021/day2.js
@@ -1,6 +1,6 @@
 import input from "./day2-input.js";
 
-const calculateResult = (arr) => {
+const calculateResultTask1 = (arr) => {
   let depth = 0;
   let horizontal = 0;
 
@@ -24,4 +24,30 @@ const calculateResult = (arr) => {
   return horizontal * depth;
 };
 
-console.log(calculateResult(input));
+const calculateResultTask2 = (arr) => {
+  let depth = 0;
+  let horizontal = 0;
+  let aim = 0;
+
+  arr.forEach((row) => {
+    const command = row.split(" ");
+    const direction = command[0];
+    const distance = parseInt(command[1]);
+
+    switch (direction) {
+      case "forward":
+        horizontal += distance;
+        depth += aim * distance;
+        break;
+      case "up":
+        aim -= distance;
+        break;
+      case "down":
+        aim += distance;
+        break;
+    }
+  });
+  return horizontal * depth;
+};
+
+console.log(calculateResultTask2(input));
