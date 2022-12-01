d815740cc03a60598dfc67ac873d4dfd00bfdec5
Dom Sip <dominik.sipowicz@gmail.com>
2022-12-01 14:30:08 +0000

    Day 1

--- aoc2022/src/day01/index.js
+++ aoc2022/src/day01/index.js
@@ -1,35 +1,109 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput) => rawInput;
+const parseInput = (rawInput) => {
+  rawInput;
+  const groups = rawInput.split(/\n/);
+
+  const elfs = [];
+
+  let count = 0;
+  for (const group of groups) {
+    if (!elfs[count]) {
+      const newElf = {
+        totalCalories: 0,
+        foodCals: [],
+      };
+      elfs[count] = newElf;
+    }
+
+    if (group.trim() === "") {
+      count++;
+    } else {
+      elfs[count].foodCals.push(parseInt(group.trim()));
+    }
+  }
+
+  return elfs;
+};
 
 const part1 = (rawInput) => {
   const input = parseInput(rawInput);
 
-  return;
+  // sum up food calories (foodCals) into totalCalories
+  for (const elf of input) {
+    elf.totalCalories = elf.foodCals.reduce((a, b) => a + b, 0);
+  }
+  // console.log(input);
+
+  // return the the elf with max totalCalories
+  const result = input.reduce((a, b) =>
+    a.totalCalories > b.totalCalories ? a : b,
+  );
+
+  return result.totalCalories;
 };
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput);
 
-  return;
+  // sum up food calories (foodCals) into totalCalories
+  for (const elf of input) {
+    elf.totalCalories = elf.foodCals.reduce((a, b) => a + b, 0);
+  }
+
+  // sort the elfs array by totalCalories
+  input.sort((a, b) => a.totalCalories - b.totalCalories);
+
+  const sumOfLest3 =
+    input.pop().totalCalories +
+    input.pop().totalCalories +
+    input.pop().totalCalories;
+
+  return sumOfLest3;
 };
 
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `1000
+        2000
+        3000
+        
+        4000
+        
+        5000
+        6000
+        
+        7000
+        8000
+        9000
+        
+        10000`,
+        expected: 24000,
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `1000
+        2000
+        3000
+        
+        4000
+        
+        5000
+        6000
+        
+        7000
+        8000
+        9000
+        
+        10000`,
+        expected: 45000,
+      },
     ],
     solution: part2,
   },
