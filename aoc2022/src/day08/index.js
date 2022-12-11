f52f87fb25bb47c8b4dac25ce1f5147840fec508
Dom Sip <dominik.sipowicz@gmail.com>
2022-12-11 14:39:22 +0000

    day 8 part 2

--- aoc2022/src/day08/index.js
+++ aoc2022/src/day08/index.js
@@ -1,7 +1,12 @@
 import run from "aocrunner";
 
 const parseInput = (rawInput) => {
-  return rawInput.split("\n").map((line) => line.trim().split(""));
+  return rawInput.split("\n").map((line) =>
+    line
+      .trim()
+      .split("")
+      .map((number) => parseInt(number, 10)),
+  );
 };
 
 const isVisible = (grid, x, y) => {
@@ -41,10 +46,102 @@ const part1 = (rawInput) => {
   return sum;
 };
 
+const getScenicScore = (grid, x, y) => {
+  // early return if on edge
+  if (x == 0 || y == 0 || x == grid.length - 1 || y == grid[0].length - 1) {
+    return 0;
+  }
+
+  const viewingDistance = {
+    up: 0,
+    down: 0,
+    left: 0,
+    right: 0,
+  };
+
+  // up direction
+  for (let i = y; i > 0; i--) {
+    viewingDistance.up += 1;
+
+    const treeHeight = grid[y][x];
+    const currentAboveTreeHeight = grid[i - 1][x];
+
+    if (currentAboveTreeHeight >= treeHeight) {
+      // the tree is higher than the current tree, so we can't see past it
+      break;
+    }
+  }
+
+  // down direction
+  for (let i = y; i < grid.length - 1; i++) {
+    viewingDistance.down += 1;
+
+    const treeHeight = grid[y][x];
+    const currentBelowTreeHeight = grid[i + 1][x];
+
+    if (currentBelowTreeHeight >= treeHeight) {
+      // the tree is higher than the current tree, so we can't see past it
+      break;
+    }
+  }
+
+  // left direction
+  for (let i = x; i > 0; i--) {
+    viewingDistance.left += 1;
+
+    const treeHeight = grid[y][x];
+    const currentLeftTreeHeight = grid[y][i - 1];
+
+    if (currentLeftTreeHeight >= treeHeight) {
+      // the tree is higher than the current tree, so we can't see past it
+      break;
+    }
+  }
+
+  // right direction
+  for (let i = x; i < grid[0].length - 1; i++) {
+    viewingDistance.right += 1;
+
+    const treeHeight = grid[y][x];
+    const currentRightTreeHeight = grid[y][i + 1];
+
+    if (currentRightTreeHeight >= treeHeight) {
+      // the tree is higher than the current tree, so we can't see past it
+      break;
+    }
+  }
+
+  // A tree's scenic score is found by multiplying together its viewing distance in each of the four directions.
+  return (
+    viewingDistance.up *
+    viewingDistance.down *
+    viewingDistance.left *
+    viewingDistance.right
+  );
+};
+
 const part2 = (rawInput) => {
   const input = parseInput(rawInput);
+  const grid = [];
 
-  return;
+  for (let y = 0; y < input.length; y++) {
+    for (let x = 0; x < input[y].length; x++) {
+      const score = getScenicScore(input, x, y);
+      grid[y] = grid[y] || [];
+      grid[y][x] = score;
+    }
+  }
+
+  // find the max score in the matrix
+  let maxScore = 0;
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (grid[y][x] > maxScore) {
+        maxScore = grid[y][x];
+      }
+    }
+  }
+  return maxScore;
 };
 
 run({
@@ -57,17 +154,22 @@ run({
 65332
 33549
 35390`,
-        expected: "21",
+        expected: 21,
       },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+30373
+25512
+65332
+33549
+35390`,
+        expected: 8,
+      },
     ],
     solution: part2,
   },
